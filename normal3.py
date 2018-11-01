28ad2adab1407fee448165fccf2ab4ae0e149aa4
ketre3 <40708384+Ketre3@users.noreply.github.com>
2018-11-01 21:51:50 +0300

    Update normal3.py

--- normal3.py
+++ normal3.py
@@ -1,10 +1,11 @@
-a = int(input('a = : '))
-b = int(input('b = : '))
-c = int(input('c = : '))
+a = int(input('a = :'))
+b = int(input('b = :'))
+c = int(input('c = :'))
 D = b**2-4*a*c
 if D < 0:
-    print('Ответ:Нет действительных корней')
+    print('Ответ: Нет действительных корней')
 else:
-    x1 = (-b+D**0.5)//2*a
-    x2 = (-b-D**0.5)//2*a
-    print('Ответ:',x1,x2)
+    x1 = (-b+D**0.5)//(2*a)
+    x2 = (-b-D**0.5)//(2*a)
+
+    print('Ответ: ', int(x1), int(x2) if x2==x1 else '')
