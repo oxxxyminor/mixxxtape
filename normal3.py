8a9246045005a8118f95dfa7cd1d42a27c2847b2
ketre3 <40708384+Ketre3@users.noreply.github.com>
2018-11-01 21:14:34 +0300

    Update normal3.py

--- normal3.py
+++ normal3.py
@@ -3,8 +3,12 @@ b = int(input('b = :'))
 c = int(input('c = :'))
 D = b**2-4*a*c
 if D < 0:
-    print('Ответ:Нет действительных корней')
+    print('Ответ: Нет действительных корней')
 else:
     x1 = (-b+D**0.5)//(2*a)
     x2 = (-b-D**0.5)//(2*a)
-    print('Ответ:', int(x1), int(x2) if x2 else '')
+
+    if x2 == x1:
+        print('Ответ: ', int(x1), int(x2))
+    else:
+        print('Ответ: ', int(x1))
